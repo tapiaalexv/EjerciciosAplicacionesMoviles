b60000a8e03865ace16b3c92552b391d0311dfc2
AlexisT <tapiaalexvpp@gmail.com>
2022-05-31 17:52:44 -0500

    Segundo cambio Mensaje Toast

--- AplicacionGit/app/src/main/java/com/tapiaalexis/aplicaciongit/MainActivity.kt
+++ AplicacionGit/app/src/main/java/com/tapiaalexis/aplicaciongit/MainActivity.kt
@@ -2,10 +2,13 @@ package com.tapiaalexis.aplicaciongit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"Hola Mundo",Toast.LENGTH_SHORT)
+            .show()
     }
 }
