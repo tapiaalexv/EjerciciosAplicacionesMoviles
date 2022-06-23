53ec274a730332cfcb0095f579ddf6c6d66ba52b
AlexisTapiaPC <tapiaalexvpp@gmail.com>
2022-06-22 21:53:07 -0500

    Pruebas de funcionalidad MainActivity.kt - Lab#3

--- Lab03_CazarPatos/app/src/main/java/com/TapiaAlexis/cazarpatos/MainActivity.kt
+++ Lab03_CazarPatos/app/src/main/java/com/TapiaAlexis/cazarpatos/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.TapiaAlexis.cazarpatos
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textViewUsuario: TextView
+    lateinit var textViewContador: TextView
+    lateinit var textViewTiempo: TextView
+    lateinit var imageViewPato: ImageView
+    var contador = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Inicialización de variables
+        textViewUsuario = findViewById(R.id.textViewUsuario)
+        textViewContador = findViewById(R.id.textViewContador)
+        textViewTiempo = findViewById(R.id.textViewTiempo)
+        imageViewPato = findViewById(R.id.imageViewPato)
 
+        //Obtener el usuario de pantalla login
+        val extras = intent.extras ?: return
+        val usuario = extras.getString(EXTRA_LOGIN) ?:"Unknown"
+        textViewUsuario.setText(usuario)
+
+        //Evento clic sobre la imagen del pato
+        imageViewPato.setOnClickListener {
+            contador++
+            MediaPlayer.create(this, R.raw.gunshot).start()
+            textViewContador.setText(contador.toString())
+        }
     }
 }
