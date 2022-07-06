82893b3966e6480c3fff43a660c4582d12a16450
AlexisTapia-Gamma <tapiaalexvpp@gmail.com>
2022-07-06 16:16:13 -0500

    Fin tercera parte - Lab 04

---
+++ Lab04_GrabarInfo/app/src/main/java/com/TapiaAlexis/cazarpatos/EncryptedSharedPreferencesManager.kt
@@ -0,0 +1,36 @@
+package com.TapiaAlexis.cazarpatos
+
+import android.app.Activity
+import android.content.Context
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+
+class EncryptedSharedPreferencesManager (val actividad: Activity): FileHandler{
+    override fun SaveInformation(datosAGrabar:Pair<String,String>){
+        val masterKeyAlias: String = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        val sharedPref = EncryptedSharedPreferences.create(
+            "secret_shared_prefs",//filename
+            masterKeyAlias,
+            actividad,//context
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+        val editor = sharedPref.edit()
+        editor.putString(LOGIN_KEY , datosAGrabar.first)
+        editor.putString(PASSWORD_KEY, datosAGrabar.second)
+        editor.apply()
+    }
+    override fun ReadInformation():Pair<String,String>{
+        val masterKeyAlias: String = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        val sharedPref = EncryptedSharedPreferences.create(
+            "secret_shared_prefs",//filename
+            masterKeyAlias,
+            actividad,//context
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+        val email = sharedPref.getString(LOGIN_KEY,"").toString()
+        val clave = sharedPref.getString(PASSWORD_KEY,"").toString()
+        return (email to clave)
+    }
+}
